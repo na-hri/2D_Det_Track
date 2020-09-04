d6dd0c8b56b909dd2960c94952cb22d9c8def5af
Nakul Agarwal <39414466+na-hri@users.noreply.github.com>
2020-09-03 21:27:44 -0700

    Delete script_videogen.py

--- deep_sort/script_videogen.py
+++
@@ -1,6 +0,0 @@
-import os
-
-splits=['s1', 's2', 's3', 's4']
-for split in splits:
-	command = 'python generate_videos.py --mot_dir /data/datasets_home/datasets/Honda_Intersection_Dataset/frames/%s --output_dir /data/datasets_home/datasets/Honda_Intersection_Dataset/frames/%s --result_dir /data/datasets_home/datasets/Honda_Intersection_Dataset/frames/%s ' % (split, split, split)
-	os.system(command)
