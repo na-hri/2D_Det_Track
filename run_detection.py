838ca7a558b58e92a606047766e5138587b4f067
Nakul Agarwal <nagarwal@honda-ri.com>
2020-09-15 13:59:46 -0700

    Update run_detection.py

--- run_detection.py
+++ run_detection.py
@@ -5,7 +5,7 @@ def main():
 	vids = [line.strip() for line in f.readlines()]
 
 	for vid in vids:
-		command = 'CUDA_VISIBLE_DEVICES=0 python Detectron.pytorch/tools/infer_simple.py --dataset coco --cfg Detectron.pytorch/e2e_mask_rcnn_X-101-64x4d-FPN_1x.yaml --load_detectron Detectron.pytorch/maskrcnn_coco_resnext.pkl --image_dir images/%s --output_dir detections/%s.txt ' % (vid,vid)
+		command = 'CUDA_VISIBLE_DEVICES=0 python Detectron.pytorch/tools/infer_simple.py --dataset coco --cfg Detectron.pytorch/e2e_mask_rcnn_X-101-64x4d-FPN_1x.yaml --load_detectron weights/maskrcnn_coco_resnext.pkl --image_dir images/%s --output_dir detections/%s.txt ' % (vid,vid)
 		os.system(command)
 		print('Video %s done' % vid)
 	f.close()
